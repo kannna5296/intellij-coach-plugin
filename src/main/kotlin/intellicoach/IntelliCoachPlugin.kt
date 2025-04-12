9ed35b53b6d27490289d47675e46e796f53fc1a9
kannna5296 <kannnahacks5296@gmail.com>
2025-04-12 15:50:19 +0900

    アクションログだけにする

--- src/main/kotlin/intellicoach/IntelliCoachPlugin.kt
+++ src/main/kotlin/intellicoach/IntelliCoachPlugin.kt
@@ -1,11 +1,16 @@
 package intellicoach
 
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseListener
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.util.messages.MessageBusConnection
 import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -16,15 +21,28 @@ class IntelliCoachPlugin {
     private val logFile = File(System.getProperty("user.home"), "intelli-coach.log")
     private val timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
-    init {
-        val multicaster = EditorFactory.getInstance().eventMulticaster
+    private var connection: MessageBusConnection? = ApplicationManager.getApplication().messageBus.connect()
 
-        multicaster.addEditorMouseListener(object : EditorMouseListener {
-            //ファイルの中をクリックしたらその内容をクリックしてくれるだけ
-            override fun mouseClicked(event: EditorMouseEvent) {
-                log("Mouse clicked: ${event.mouseEvent.point}")
+    init {
+        connection?.subscribe(
+            AnActionListener.TOPIC,
+            object : AnActionListener {
+                override fun beforeActionPerformed(action: AnAction, event: AnActionEvent) {
+                    val actionText = action.templatePresentation.text
+                    when (event.inputEvent) {
+                        is java.awt.event.MouseEvent -> {
+                            log("⚡ Action triggered: $actionText by mouse")
+                        }
+                        is java.awt.event.KeyEvent -> {
+                            log("⚡ Action triggered: $actionText by shortcut")
+                        }
+                        else -> {
+                            log("⚡ Action triggered: $actionText by mouse")
+                        }
+                    }
+                }
             }
-        }, ApplicationManager.getApplication())
+        )
     }
 
     private fun log(message: String) {
--- src/main/kotlin/intellicoach/IntelliCoachStartupActivity.kt
+++ src/main/kotlin/intellicoach/IntelliCoachStartupActivity.kt
@@ -1,18 +1,11 @@
 package intellicoach
 
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 
 class MyStartupActivity : ProjectActivity {
-    private val logger = Logger.getInstance(MyStartupActivity::class.java)
-
     override suspend fun execute(project: Project) {
-        logger.info("✅ MyStartupActivity: 起動しました！")
-        println("✅ MyStartupActivity: 起動しました！")
-
         // ここで明示的にロードすることで IntelliCoachPlugin の init が呼ばれる
         val plugin = project.getService(IntelliCoachPlugin::class.java)
-        println("👀 IntelliCoachPlugin loaded by MyStartupActivity: $plugin")
     }
 }
