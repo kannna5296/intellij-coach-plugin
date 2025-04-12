c12dc22ec0d1f59f0b6edd01eae3e72aa2cb62db
kannna5296 <kannnahacks5296@gmail.com>
2025-04-12 15:37:16 +0900

    ログに出るようにする

--- src/main/kotlin/intellicoach/IntelliCoachPlugin.kt
+++ src/main/kotlin/intellicoach/IntelliCoachPlugin.kt
@@ -6,24 +6,29 @@ import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseListener
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.application.ApplicationManager
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Service(Service.Level.APP)
 class IntelliCoachPlugin {
 
-    private val logger = Logger.getInstance(IntelliCoachPlugin::class.java)
+    private val logFile = File(System.getProperty("user.home"), "intelli-coach.log")
+    private val timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
     init {
-        logger.info("✅ IntelliCoachPlugin loaded!")
-        println("✅ IntelliCoachPlugin loaded!")
-
         val multicaster = EditorFactory.getInstance().eventMulticaster
 
         multicaster.addEditorMouseListener(object : EditorMouseListener {
             //ファイルの中をクリックしたらその内容をクリックしてくれるだけ
             override fun mouseClicked(event: EditorMouseEvent) {
-                logger.info("🖱️ Mouse clicked: ${event.mouseEvent.point}")
-                println("🖱️ Mouse clicked: ${event.mouseEvent.point}")
+                log("Mouse clicked: ${event.mouseEvent.point}")
             }
         }, ApplicationManager.getApplication())
+    }
+
+    private fun log(message: String) {
+        val timestamp = LocalDateTime.now().format(timeFormatter)
+        logFile.appendText("[$timestamp] $message\n")
     }
 }
