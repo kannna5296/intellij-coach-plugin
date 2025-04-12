b0b87edd450c26008f4f7b31f184be6e76f8b298
kannna5296 <kannnahacks5296@gmail.com>
2025-04-12 14:17:59 +0900

    非推奨の修正

--- src/main/kotlin/intellicoach/IntelliCoachStartupActivity.kt
+++ src/main/kotlin/intellicoach/IntelliCoachStartupActivity.kt
@@ -2,12 +2,12 @@ package intellicoach
 
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 
-class MyStartupActivity : StartupActivity {
+class MyStartupActivity : ProjectActivity {
     private val logger = Logger.getInstance(MyStartupActivity::class.java)
 
-    override fun runActivity(project: Project) {
+    override suspend fun execute(project: Project) {
         logger.info("✅ MyStartupActivity: 起動しました！")
         println("✅ MyStartupActivity: 起動しました！")
     }
