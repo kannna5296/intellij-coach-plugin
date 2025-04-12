21b7a8f6e79c3435ed58b1b3e6d363b6e2a744ee
kannna5296 <kannnahacks5296@gmail.com>
2025-04-12 15:59:50 +0900

    format

--- src/main/kotlin/intellicoach/IntelliCoachStartupActivity.kt
+++ src/main/kotlin/intellicoach/IntelliCoachStartupActivity.kt
@@ -5,7 +5,6 @@ import com.intellij.openapi.startup.ProjectActivity
 
 class IntelliCoachStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
-        // ここで明示的にロードすることで IntelliCoachPlugin の init が呼ばれる
         val plugin = project.getService(IntelliCoachPlugin::class.java)
     }
 }
